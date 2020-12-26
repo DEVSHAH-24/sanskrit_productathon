b08fe242b595b3b992cfdd6fa510cb4c8b276a49
Addepalli N M PavanKalyan <57014239+pavankalyan0424@users.noreply.github.com>
2020-12-26 22:33:39 +0530

    user collection code added

---
+++ lib/models/dataModel.dart
@@ -0,0 +1,13 @@
+class DataModel {
+  final String name;
+  final String email;
+  final String photoUrl;
+  final String userId;
+
+  DataModel({
+    this.name,
+    this.email,
+    this.photoUrl,
+    this.userId,
+  });
+}
--- lib/models/firebaseModel.dart
+++ lib/models/firebaseModel.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:sanskrit_project/models/dataModel.dart';
 
 class FirebaseModel {
   static const String projectName = 'sanskrit';
@@ -7,7 +9,8 @@ class FirebaseModel {
   static const String projectId = 'sanskrit-f24c2';
   static const String apiKey = 'AIzaSyAqdnYH3NngvrpLB4a0MsNnZIZHOXmreg4';
   static const String appId = '1:68627785713:android:39ee8d5ca584fb837e0650';
-  static FirebaseFirestore _db;
+  static FirebaseFirestore _db = FirebaseFirestore.instance;
+  static DocumentReference ref;
 
   Future<void> initializeFirebase() async {
     await Firebase.initializeApp(
@@ -21,4 +24,46 @@ class FirebaseModel {
     );
   }
 
+  void initializeCollection(String userId) {
+    ref = _db.collection('users').doc(userId);
+  }
+
+  Future<void> uploadUserData(User user) async {
+    initializeCollection(user.uid);
+    await ref.set(
+        {
+          'name': user.displayName,
+          'email': user.email,
+          'photoUrl': user.photoURL,
+          'userId': user.uid,
+        },
+        SetOptions(
+          merge: true,
+        ));
+  }
+
+  Future<DataModel> getUserDataFromUser(User user) async {
+    initializeCollection(user.uid);
+    DataModel data = DataModel(
+      name: user.displayName,
+      email: user.email,
+      photoUrl: user.photoURL,
+      userId: user.uid,
+    );
+    return data;
+  }
+
+  Future<DataModel> getUserDataFromCloud(String userId) async {
+    initializeCollection(userId);
+    DataModel data = DataModel();
+    await ref.get().then((documentSnapshot) {
+      data = DataModel(
+        name: documentSnapshot.data()['name'],
+        email: documentSnapshot.data()['email'],
+        photoUrl: documentSnapshot.data()['photoUrl'],
+        userId: documentSnapshot.data()['userId'],
+      );
+    });
+    return data;
+  }
 }
