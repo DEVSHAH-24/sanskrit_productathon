a057abebf5ea74f3542484f5507650df70b809f6
DEVSHAH-24 <devshah2401@gmail.com>
2021-01-03 02:28:56 +0530

    minor change

--- lib/pages/messaging.dart
+++ lib/pages/messaging.dart
@@ -120,7 +120,7 @@ class MessageStream extends StatelessWidget {
             ),
           );
         }
-        final messages = snapshot.data.docs;
+        final messages = snapshot.data.docs.reversed;
         List<MessageBubble> messageBubbles = [];
         for (var message in messages) {
           final messageSenderName = message.data()['username'] != null
